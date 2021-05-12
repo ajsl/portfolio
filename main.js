b2aa04bb732af51a9c972045aaf655ad4d471198
James Leith <james@Jamess-MacBook-Pro-2.local>
2021-05-12 10:03:26 -0700

    added css colour variables removed unused photos

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ const debounce = (fn) => {
 
 const storeScroll = () => {
   document.documentElement.dataset.scroll = window.scrollY;
-  console.log(window.scrollY);
 };
 
 document.addEventListener("scroll", debounce(storeScroll), { passive: true });
